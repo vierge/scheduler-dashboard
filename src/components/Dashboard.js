bd9d53652d6a8ab25df03080a244ea49ce8082ff
vierge <marancholia@gmail.com>
2020-05-26 11:20:11 -0400

    implemented dashboard. todo: hook to api

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,84 @@
 import React, { Component } from "react";
-
+import Loading from "components/Loading";
+import Panel from "components/Panel";
 import classnames from "classnames";
 
-class Dashboard extends Component {
-  state = { loading: true };
-  render() {
-    const dashboardClasses = classnames("dashboard");
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6,
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm",
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday",
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3",
+  },
+];
 
-    return <main className={dashboardClasses} />;
+class Dashboard extends Component {
+  state = {
+    days: [],
+    appointments: {},
+    interviewers: {},
+    loading: true,
+    focused: null,
+  };
+
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
+
+    if (focused) {
+      this.setState({ focused });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  }
+
+  selectPanel(id) {
+    this.setState((previousState) => ({
+      focused: previousState.focused !== null ? null : id,
+    }));
+  }
+
+  render() {
+    const getPanels = data
+      .filter(
+        (panel) =>
+          this.state.focused === null || this.state.focused === panel.id
+      )
+      .map((panel) => {
+        return (
+          <Panel
+            key={panel.id}
+            id={panel.id}
+            label={panel.label}
+            value={panel.value}
+            onSelect={(event) => this.selectPanel(panel.id)}
+          />
+        );
+      });
+
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused,
+    });
+
+    if (this.state.loading) return <Loading />;
+    return <main className={dashboardClasses}>{getPanels}</main>;
   }
 }
 
